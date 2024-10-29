f4719a58c829fae10b11710c38dbe105f9866cc3
AdiTailor <aditya.tailor@outlook.com>
2024-10-29 11:33:25 +0530

    Implement DatePickerFragment and TimePickerFragment

--- app/src/main/java/androidsamples/java/journalapp/DatePickerFragment.java
+++ app/src/main/java/androidsamples/java/journalapp/DatePickerFragment.java
@@ -4,24 +4,52 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public class DatePickerFragment extends DialogFragment {
+
+  private static final String ARG_DATE = "date";
+
+  public interface DatePickerListener {
+    void onDateSelected(Date date);
+  }
   @NonNull
   public static DatePickerFragment newInstance(Date date) {
     // TODO implement the method
-    return null;
+    DatePickerFragment fragment = new DatePickerFragment();
+    Bundle args = new Bundle();
+    args.putLong(ARG_DATE, date != null ? date.getTime() : System.currentTimeMillis());
+    fragment.setArguments(args);
+    return fragment;
   }
 
   @NonNull
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
     // TODO implement the method
+    long dateInMillis = getArguments() != null ? getArguments().getLong(ARG_DATE) : System.currentTimeMillis();
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(dateInMillis);
 
-    return new DatePickerDialog(requireContext(), (dp, y, m, d) -> {
-    }, 0, 0, 0);
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    return new DatePickerDialog(requireContext(), (datePicker, selectedYear, selectedMonth, selectedDay) -> {
+      Calendar selectedCalendar = Calendar.getInstance();
+      selectedCalendar.set(Calendar.YEAR, selectedYear);
+      selectedCalendar.set(Calendar.MONTH, selectedMonth);
+      selectedCalendar.set(Calendar.DAY_OF_MONTH, selectedDay);
+      Date selectedDate = selectedCalendar.getTime();
+      if (getParentFragment() instanceof DatePickerListener) {
+        ((DatePickerListener) getParentFragment()).onDateSelected(selectedDate);
+      } else if (getActivity() instanceof DatePickerListener) {
+        ((DatePickerListener) getActivity()).onDateSelected(selectedDate);
+      }
+    }, year, month, day);
   }
 }
--- app/src/main/java/androidsamples/java/journalapp/TimePickerFragment.java
+++ app/src/main/java/androidsamples/java/journalapp/TimePickerFragment.java
@@ -3,7 +3,10 @@ package androidsamples.java.journalapp;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import androidx.annotation.NonNull;
@@ -11,23 +14,50 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class TimePickerFragment extends DialogFragment {
+  private static final String ARG_TIME = "time";
 
+  public interface OnTimeSelectedListener {
+    void onTimeSelected(int hourOfDay, int minute);
+  }
+  private OnTimeSelectedListener mListener;
   @NonNull
   public static TimePickerFragment newInstance(Date time) {
     // TODO implement the method
-    return null;
+    Bundle args = new Bundle();
+    args.putSerializable(ARG_TIME, time);
+
+    TimePickerFragment fragment = new TimePickerFragment();
+    fragment.setArguments(args);
+    return fragment;
   }
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     // TODO implement the method
+    return ;
+  }
+
+  public void setOnTimeSelectedListener(OnTimeSelectedListener listener) {
+    mListener = listener;
   }
 
   @NonNull
   @Override
   public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
     // TODO implement the method
-    return new TimePickerDialog(requireContext(), (tp, hm, m)->{}, 0, 0, false);
+    Calendar calendar = Calendar.getInstance();
+    if (getArguments() != null) {
+      calendar = (Calendar) getArguments().getSerializable(ARG_TIME);
+    }
+    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+    int minute = calendar.get(Calendar.MINUTE);
+    return new TimePickerDialog(requireContext(), this::onTimeSet, hour, minute, DateFormat.is24HourFormat(requireContext()));
+  }
+
+  private void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+    if (mListener != null) {
+      mListener.onTimeSelected(hourOfDay, minute);
+    }
   }
 }
