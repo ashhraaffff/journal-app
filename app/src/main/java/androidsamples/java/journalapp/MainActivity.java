a8ae6674992f785a7a7572595215e51a507cba28
AdiTailor <aditya.tailor@outlook.com>
2024-10-29 12:19:46 +0530

    Implement MainActivity

--- app/src/main/java/androidsamples/java/journalapp/MainActivity.java
+++ app/src/main/java/androidsamples/java/journalapp/MainActivity.java
@@ -3,14 +3,28 @@ package androidsamples.java.journalapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+  private NavController mNavController;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.nav_host_fragment);
+    if (navHostFragment != null) {
+      mNavController = navHostFragment.getNavController();
+      NavigationUI.setupActionBarWithNavController(this, mNavController);
+    }
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    return mNavController.navigateUp() || super.onSupportNavigateUp();
   }
 }
